f8ec8a4bea6bc1d9b50378dea02d8ef0d7fbf3fd
Paul <paulmessmer1s@semo.edu>
2022-10-23 23:43:44 -0500

    Changes to be committed: 	new file:   api/.gitignore 	new file:   api/index.js 	new file:   api/package-lock.json 	new file:   api/package.json 	new file:   package-lock.json 	modified:   package.json

---
+++ api/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+// const helmet = require("helmet");
+// const morgan = require("morgan");
+const path = require("path");
+//const routes = require("./routes");
+
+const app = express();
+
+// app.use(helmet());
+
+// Sets up request body JSON parsing
+app.use(express.json());
+
+// Tells express about the client folder
+app.use(express.static(path.join(__dirname, "../client")));
+
+// index html
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
+
+// Sends a 404 error if no end points match the end points.
+app.use((req, res) => {
+	res.status(404).json({
+		message: "Route Not Found",
+	});
+});
+
+// Sets the PORT variable to either the port it is hosted on through Heroku OR port 500
+const PORT = process.env.PORT || 5001;
+
+// Starts listening on the PORT variable
+app.listen(PORT, () => {
+	console.log(`The application is running on ${PORT}`);
+});
